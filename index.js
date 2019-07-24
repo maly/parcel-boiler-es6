5573d810e94330e54241f2dc2d1b7e88c1d09d21
Martin Maly <maly@maly.cz>
2019-07-24 12:00:03 +0200

    Base files

--- index.js
+++ index.js
@@ -16,6 +16,29 @@ pkg.scripts.dev = "parcel index.html";
 pkg.scripts.build = "rm build/* ; parcel build index.html -d build --public-url /offline/ --no-source-maps";
 fs.writeFileSync(cwd + "/package.json", JSON.stringify(pkg, null, 4))
 
+//main.js
+var mainjs = 'window.$ = window.jQuery = require("./node_modules/jquery/dist/jquery.min.js");\n'
+mainjs += 'require("./node_modules/bootstrap/dist/js/bootstrap.min");\n'
+mainjs += 'require("jquery-ui-dist/jquery-ui.js");\n'
+
+
+fs.writeFileSync(cwd + "/main.js", mainjs)
+
+var indexhtml = '';
+indexhtml += '<!DOCTYPE html>\n'
+indexhtml += '<html>\n'
+indexhtml += '<head>\n'
+indexhtml += '    <meta charset=utf-8>\n'
+indexhtml += '    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">\n'
+indexhtml += '    <meta name="description" content="">\n'
+indexhtml += '    <meta name="author" content="">\n'
+
+indexhtml += '    <link href="./node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">\n'
+indexhtml += '    <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.css">\n'
+indexhtml += '</head>\n'
+indexhtml += '<body>\n\n</body>\n</html>'
+
+fs.writeFileSync(cwd + "/index.html", indexhtml)
 
 const donpm = function (npm) {
 
